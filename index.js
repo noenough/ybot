4eb5609f3e7b74210581bb3d03421867003e1772
Delisional <annamuhammedovyhlas005@gmail.com>
2025-06-25 12:56:26 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const axios = require('axios')
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.write("SALAM");
+    res.end();
+})
+
+app.listen(8080);
